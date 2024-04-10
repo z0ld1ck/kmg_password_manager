b10025b9269cf50313e503dbf3520849c58a5664
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2024-04-10 23:09:20 +0500

    router was added

--- lib/core/navigation/router.dart
+++ lib/core/navigation/router.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
-import 'package:kmg_password_manager/views/auth_view/login_page_view/page/login_page_view.dart';
-import 'package:kmg_password_manager/views/auth_view/registration_page_view/page/registration_page_view.dart';
 import 'package:kmg_password_manager/welcomepage.dart';
-import '../../views/home_view/page/homepage_view.dart';
+import '../../features/auth/presentation/login/page/login_page_view.dart';
+import '../../features/auth/presentation/registration/page/registration_page_view.dart';
+import '../../features/home/presentation/page/homepage_view.dart';
+import '../../features/profile/presentation/page/profile_page_view.dart';
 import 'route_names.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey =
@@ -14,12 +15,7 @@ final GoRouter goRouter = GoRouter(
   routes: <RouteBase>[
     GoRoute(
       path:'/',
-      name:RouteNames.welcome.name,
-      builder: (context, state) => const WelcomePage(),
-    ),
-    GoRoute(
-      path: RouteNames.home.path,
-      name: RouteNames.home.name,
+      name:RouteNames.home.name,
       builder: (context, state) => const HomePageView(),
     ),
     GoRoute(
@@ -28,9 +24,19 @@ final GoRouter goRouter = GoRouter(
       builder: (context, state) => const LoginPageView(),
     ),
     GoRoute(
+      path: RouteNames.welcome.path,
+      name: RouteNames.welcome.name,
+      builder: (context, state) => const WelcomePage(),
+    ),
+    GoRoute(
       path: RouteNames.registration.path,
       name: RouteNames.registration.name,
       builder: (context, state) => const RegistrationPageView(),
+    ),
+    GoRoute(
+      path: RouteNames.profile.path,
+      name: RouteNames.profile.name,
+      builder: (context, state) => const ProfilePageView(),
     ),
   ],
 );
