93eac62af283e2483de08b9f7e2458bb3f78b5b4
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2024-04-10 23:08:49 +0500

    constants was added

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(375, 812),
+        designSize: const Size(360, 690),
         minTextAdapt: true,
         splitScreenMode: true,
         builder: (_, child) {
---
+++ lib/core/const/app_icons.dart
@@ -0,0 +1,4 @@
+class AppIcons {
+  static const String visible = 'assets/icons/visibility.svg';
+  static const String invisible = 'assets/icons/visibility_off.svg';
+}
