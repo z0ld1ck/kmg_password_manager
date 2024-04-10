a135de5ae2b23f0f5941f45a59fd7803e78aecd5
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2024-04-10 23:14:44 +0500

    user model was added

---
+++ lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+
+part 'user_model.freezed.dart';
+part 'user_model.g.dart';
+
+
+@freezed
+class UserModel with _$UserModel {
+  @JsonSerializable(fieldRename: FieldRename.snake)
+  const factory UserModel({
+    required int id,
+    required String username,
+    required String? password,
+  }) = _UserModel;
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
+}
+
---
+++ lib/features/auth/data/models/user_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserModelImpl _$$UserModelImplFromJson(Map<String, dynamic> json) =>
+    _$UserModelImpl(
+      id: json['id'] as int,
+      username: json['username'] as String,
+      password: json['password'] as String?,
+    );
+
+Map<String, dynamic> _$$UserModelImplToJson(_$UserModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'username': instance.username,
+      'password': instance.password,
+    };
